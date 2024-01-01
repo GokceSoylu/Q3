69addbec39c53f2ba35c2d9032f62123fca1cdc7
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-02 00:50:03 +0300

    form submit button alert js added

---
+++ Arizali_Musluk_Kalmasin/JavaScript/button_form.js
@@ -0,0 +1,4 @@
+const the_bottun=document.querySelector("#submit_arizaForm");
+document.addEventListener('click',function(){
+    alert("Arızanız kaydedildi. \n\nSize en kısa zamanda döneceğiz")
+});
