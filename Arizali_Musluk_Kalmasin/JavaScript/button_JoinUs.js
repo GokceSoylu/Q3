925f305806c0e7a3c5e8af95855a99bab8b0eb91
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-02 07:14:31 +0300

    click event added

---
+++ Arizali_Musluk_Kalmasin/JavaScript/button_JoinUs.js
@@ -0,0 +1,5 @@
+const join_us_button = document.querySelector("#join_us");
+join_us_button.addEventListener("click",the_function);
+function the_function(){
+    alert("Gelmeyin arkadaşlar.\nTünelin ucu bombok bi yere çıkıyor");
+}//dikkat edelim tipinin function olduğunu tanımaldık
--- Arizali_Musluk_Kalmasin/JavaScript/button_form.js
+++ Arizali_Musluk_Kalmasin/JavaScript/button_form.js
@@ -1,4 +1,6 @@
-const the_bottun=document.querySelector("#submit_arizaForm");
-document.addEventListener('click',function(){
+const the_button=document.querySelector("#submit_arizaForm");
+the_button.addEventListener('click',function(){
     alert("Arızanız kaydedildi. \n\nSize en kısa zamanda döneceğiz")
-});
+});//şimdi hocam buradaki olayı anladık değil mi önce değişken oluşturuyoruz ve querySelector ile istediğimiz elemanı seçiyoruz
+//değişkenimize bunu atıyoruz. aşşagida bu isteğimiz elemanı atadığımız objeyle metodumnmuzu çalıştırıyoruz. 
+//metod iki tane parametre alıcak biri event in türü diğeri fonksiyonun edı aslında kendisi :))
