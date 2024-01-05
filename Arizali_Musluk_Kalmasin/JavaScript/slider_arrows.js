088f31935c497ffd7f38d2bbf63ce5808014683f
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-06 01:14:00 +0300

    start to slider js

---
+++ Arizali_Musluk_Kalmasin/JavaScript/slider_arrows.js
@@ -0,0 +1,6 @@
+const left_arrow=document.querySelector("#to_left");
+const right_arrow=document.querySelector("to_right");
+const area=document.querySelector("#slider > ul");
+const area_total_width=area.scrollWidth;
+const one_part=area.querySelector("li").clientWidth;
+
