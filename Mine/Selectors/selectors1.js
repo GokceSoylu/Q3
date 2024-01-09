a08079876d5be275269c152a02a84462e528e4e7
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-09 23:59:39 +0300

    selectors

--- Mine/Selectors/selectors1.js
+++ Mine/Selectors/selectors1.js
@@ -1,3 +1,10 @@
+//! Selectors
+//classname, id, tag bilgilerine ulaşabılırıiz
+//getEleemntById ile id ile eleemneti yakalar
+
 let value;
-value=document.getElementById("#hi_button");
+value=document.getElementById("the_button");
+console.log(value);
 console.log(value.id);
+console.log("second ",value.getAttribute("id"));
+console.log(value.className);
