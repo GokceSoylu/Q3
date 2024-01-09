6a117aa66bbbf3618edaebe2647cd476ab774ac5
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-09 11:22:43 +0300

    olmadı bırakıyorum :)

--- Arizali_Musluk_Kalmasin/JavaScript/slider_arrows.js
+++ Arizali_Musluk_Kalmasin/JavaScript/slider_arrows.js
@@ -4,3 +4,43 @@ const area=document.querySelector("#slider > ul");
 const area_total_width=area.scrollWidth;
 const one_part=area.querySelector("li").clientWidth;
 
+left_arrow.addEventListener("click",event=>
+{
+    if(area.scrollLeft===0)
+    {
+        area.scrollTo
+        ({
+            top:0,
+            left:area_total_width,
+            behavior:"smooth"
+        } )
+    }
+    else
+    {
+        area.scrollTo
+        ({
+            top:0,
+            left:area.scrollLeft - 3*one_part,
+            behavior:"smooth"
+        })
+    }
+})
+
+right_arrow.addEventListener("click",event=>{
+    if(area.scrollLeft>=area_total_width)
+    {
+        area.scrollTo
+        ({
+            top:0,
+            left:0,
+            behavior:"smooth"
+       })
+    }
+    else{
+        area.scrollTo({
+            top:0,
+            left:area_total_width + 3*one_part,
+            behavior:"smooth"
+        })
+    }
+})
