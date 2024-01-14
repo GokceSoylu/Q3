2e654cfd9e26fe41edc4c0ab9d90354010c5cbe5
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-14 21:41:29 +0300

    dblclick event

---
+++ Mine/events/mouseEvents/dbl_button.js
@@ -0,0 +1,11 @@
+let dbl_click=document.querySelector("#dbl_button");
+
+dbl_click.addEventListener("dblclick",event=>
+{
+    alert("MY NAME :))");
+})
+
+dbl_click=addEventListener("click",event=>
+{
+    alert("YOUR NAME");
+})
